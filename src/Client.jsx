9ae377c0fb7ca3dc5be65dacbac5136c7f1e8d03
Ismaila Diallo <50891200+ismaila10@users.noreply.github.com>
2021-01-08 03:14:37 +0100

    Composant fonctionnel

--- src/Client.jsx
+++ src/Client.jsx
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
 
-class Client extends Component {
-  render() {
-    const { details, onDelete } = this.props;
-
-    return (
-      <li>
-        {details.nom} <button onClick={() => onDelete(details.id)}>X</button>
-      </li>
-    );
-  }
-}
+const Client = ({ details, onDelete }) => (
+  <li>
+    {details.nom} <button onClick={() => onDelete(details.id)}>X</button>
+  </li>
+);
 
 export default Client;
